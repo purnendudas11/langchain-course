368761cda7d7600b0d12ddced6f240f96deddbab
Purnendu Das <purnendu96.das@gmail.com>
2026-07-08 09:42:15 -0500

    added openAI llm

--- main.py
+++ main.py
@@ -5,35 +5,55 @@ from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_aws import ChatBedrock
 from tavily import TavilyClient
-# from langchain_tavily import TavilySearch 
+from langchain_tavily import TavilySearch 
+from typing import List
+from pydantic import BaseModel, Field
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
 tavily=TavilyClient()
 
-@tool
-def search(query: str) -> str:
-    """ 
-    A simple search tool that returns a string indicating the search results for the given query 
-    from internet.
-    Args:
-        query (str): The search query.
-    Returns:
-        str: A string indicating the search results for the given query.
-    """
-    print(f"Search results for '{query}'")
-    return tavily.search(query=query)
+# @tool
+# def search(query: str) -> str:
+#     """ 
+#     A simple search tool that returns a string indicating the search results for the given query 
+#     from internet.
+#     Args:
+#         query (str): The search query.
+#     Returns:
+#         str: A string indicating the search results for the given query.
+#     """
+#     print(f"Search results for '{query}'")
+#     return tavily.search(query=query)
 
-llm = ChatBedrock(
-    model_id="amazon.nova-pro-v1:0",
-    region_name=os.getenv("AWS_DEFAULT_REGION"),
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+class Source(BaseModel):
+    """Schema for source of information used by agent to answer the question"""
+    url: str = Field(..., description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response"""
+    answer: str = Field(..., description="The answer to the question")
+    sources: List[Source] = Field(..., description="The sources of information used to answer the question")
+
+
+# llm = ChatBedrock(
+#     model_id="amazon.nova-pro-v1:0",
+#     region_name=os.getenv("AWS_DEFAULT_REGION"),
+#     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+#     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+#     temperature=0
+# )
+
+llm = ChatOpenAI(
+    model_name="gpt-4o",
+    openai_api_key=os.getenv("OPENAI_API_KEY"),
     temperature=0
 )
-tools = [search]
-# tools = [TavilySearch()]
-agent = create_agent(llm, tools)
+
+# tools = [search]
+tools = [TavilySearch()]
+agent = create_agent(llm, tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
